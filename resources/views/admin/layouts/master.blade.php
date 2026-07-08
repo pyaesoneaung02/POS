1863d263769c1a8bd8cfae478c91084ee2b17b12
PSA <psoneaung965.com>
2026-07-08 20:25:40 +0630

    Deploy Laravel Project

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -36,7 +36,7 @@
                 <div class="sidebar-brand-icon rotate-n-15">
                     <i class="fas fa-laugh-wink"></i>
                 </div>
-                <div class="sidebar-brand-text mx-3">Code Lab Studio</div>
+                <div class="sidebar-brand-text mx-3">POS System</div>
             </a>
 
             <!-- Divider -->
