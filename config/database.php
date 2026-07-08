b4c659b167bc5f2553e999f6131ffbc97cd48e4f
PSA <psoneaung965.com>
2026-07-08 17:35:43 +0630

    Commit to laravel project

--- config/database.php
+++ config/database.php
@@ -42,46 +42,25 @@ return [
             'synchronous' => null,
         ],
 
-        // 'mysql' => [
-        //     'driver' => 'mysql',
-        //     'url' => env('DB_URL'),
-        //     'host' => env('DB_HOST', '127.0.0.1'),
-        //     'port' => env('DB_PORT', '3306'),
-        //     'database' => env('DB_DATABASE', 'laravel'),
-        //     'username' => env('DB_USERNAME', 'root'),
-        //     'password' => env('DB_PASSWORD', ''),
-        //     'unix_socket' => env('DB_SOCKET', ''),
-        //     'charset' => env('DB_CHARSET', 'utf8mb4'),
-        //     'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
-        //     'prefix' => '',
-        //     'prefix_indexes' => true,
-        //     'strict' => false,
-        //     'engine' => null,
-        //     'options' => extension_loaded('pdo_mysql') ? array_filter([
-        //         PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-        //     ]) : [],
-        // ],
-
-'mysql' => [
-    'driver'      => 'mysql',
-    'url'         => env('DATABASE_URL'),
-    'host'        => env('DB_HOST'),
-    'port'        => env('DB_PORT', '3306'),
-    'database'    => env('DB_DATABASE'),
-    'username'    => env('DB_USERNAME'),
-    'password'    => env('DB_PASSWORD'),
-    'unix_socket' => env('DB_SOCKET', ''),
-    'charset'     => 'utf8mb4',
-    'collation'   => 'utf8mb4_unicode_ci',
-    'prefix'      => '',
-    'strict'      => true,
-    'engine'      => null,
-
-    'options'     => extension_loaded('pdo_mysql') ? [
-        PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-    ] : [],
-],
-
+        'mysql' => [
+            'driver' => 'mysql',
+            'url' => env('DB_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'laravel'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => env('DB_CHARSET', 'utf8mb4'),
+            'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => false,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
 
         'mariadb' => [
             'driver' => 'mariadb',
@@ -132,10 +111,6 @@ return [
             // 'encrypt' => env('DB_ENCRYPT', 'yes'),
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
-
-        // 'options' => extension_loaded('pdo_mysql') ? array_filter([
-        //     PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-        // ]) : [],
 
     ],
 
